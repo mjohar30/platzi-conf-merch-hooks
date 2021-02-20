50da685e6f22f22fbfd78b44624de71ff510da3c
Mario Johar <mjohar30@gmail.com>
2021-02-20 14:44:14 -0600

    Information, Payment and Success components

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../styles/components/Success.css'
 
 const Success = () => {
   return (
-    <div>
-      Success
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Johar, Gracias por tu compra</h2>
+        <span>Tu pedido llegará en 3 días a tu dirección:</span>
+        <div className="Success-map">
+          Google Maps
+        </div>
+      </div>
     </div>
   );
 };
