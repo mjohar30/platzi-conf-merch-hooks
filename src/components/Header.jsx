5bad609eeb258520e40baf1914bed57ca407e939
Mario Johar <mjohar30@gmail.com>
2021-02-20 18:35:10 -0600

    customHook (useInitialState) and useContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom'
+import AppContext from '../context/AppContext'
 import '../styles/components/Header.css'
 
 const Header = () => {
+
+  const { state: { cart } } = useContext(AppContext)
+
   return (
     <div className="Header">
       <h1 className="Header-title">
@@ -14,6 +18,7 @@ const Header = () => {
         <Link to="/checkout">
           <i className="fas fa-shopping-basket fa-2x" title="Checkout"></i>
         </Link>
+        {cart.length > 0 && <div className="Header-alert">{cart.length}</div>}
       </div>
     </div>
   );
