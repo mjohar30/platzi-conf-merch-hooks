5b264bd8597abb154eb54b26624d8ea02adc9e93
Mario Johar <mjohar30@gmail.com>
2021-03-11 16:27:46 -0600

    Add react-helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet'
 import AppContext from '../context/AppContext'
 import { handleSumTotal } from './../utils'
 import '../styles/components/Checkout.css'
@@ -12,30 +13,35 @@ const Checkout = () => {
   }
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        {cart.length > 0 ? <h3>Lista de pedidos</h3> : <h3>Sin pedidos...</h3>}
-        {cart.map(item => (
-          <div className="Checkout-item">
-            <div className="Checkout-element">
-              <h4>{item.title}</h4>
-              <span>${item.price}</span>
+    <>
+      <Helmet>
+        <title>Lista de pedidos - Platzi Conf Merch</title>
+      </Helmet>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          {cart.length > 0 ? <h3>Lista de pedidos</h3> : <h3>Sin pedidos...</h3>}
+          {cart.map(item => (
+            <div className="Checkout-item">
+              <div className="Checkout-element">
+                <h4>{item.title}</h4>
+                <span>${item.price}</span>
+              </div>
+              <button type="button" onClick={handleRemove(item)}>
+                <i className="fas fa-trash-alt fa-2x" title="Eliminar"></i>
+              </button>
             </div>
-            <button type="button" onClick={handleRemove(item)}>
-              <i className="fas fa-trash-alt fa-2x" title="Eliminar"></i>
-            </button>
-          </div>
-        ))}
-      </div>
-      {cart.length > 0 && (
-        <div className="Checkout-sidebar">
-          <h3>{`Precio Total: $ ${handleSumTotal(cart)}`}</h3>
-          <Link to="/checkout/information">
-            <button type="button">Continuar pedido</button>
-          </Link>
+          ))}
         </div>
-      )}
-    </div>
+        {cart.length > 0 && (
+          <div className="Checkout-sidebar">
+            <h3>{`Precio Total: $ ${handleSumTotal(cart)}`}</h3>
+            <Link to="/checkout/information">
+              <button type="button">Continuar pedido</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'
 import Products from '../components/Products'
 
 const Home = () => {
   return (
-    <Products/>
+    <>
+      <Helmet>
+        <title>Platzi Conf Merch - Productos</title>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:site" content="@JoharGp"/>
+        <meta name="twitter:creator" content="@JoharGp"/>
+        <meta name="twitter:title" content="Platzi Conf Store"/>
+        <meta name="twitter:description" content="Platzi Conf Store"/>
+        <meta
+          name="twitter:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:title" content="Platzi Conf Store"/>
+        <meta property="og:description" content="Platzi Conf Store"/>
+        <meta
+          property="og:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:url" content="platzi-conf-merch-hooks.web.app/" />
+        <meta property="og:site_name" content="Platzi Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+      </Helmet>
+      <Products/>
+    </>
   );
 };
 
